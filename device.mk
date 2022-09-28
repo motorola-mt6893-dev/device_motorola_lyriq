43db157f1fcacb4c6e2d2f3827d6d5d09d38797f
Luca Stefani <luca.stefani.ge1@gmail.com>
2022-09-28 19:17:19 +0100

    lyriq: Enable updateable APEX
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Enable Updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Enable Virtual A/B OTA with vendor ramdisk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
