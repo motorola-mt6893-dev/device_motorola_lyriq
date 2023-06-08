44a1638178087f2e427c27e1cd8616ba9c4478e8
Sushrut1101 <guptasushrut@gmail.com>
2023-06-09 00:50:38 +0530

    lyriq: Inherit common MediaTek vendor SEPolicy
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -141,6 +141,9 @@ TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
+# SEPolicy
+include device/mediatek/sepolicy_vndr/SEPolicy.mk
+
 # VNDK
 BOARD_VNDK_VERSION := current
 
